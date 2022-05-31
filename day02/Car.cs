5031d114652dc3321f35f83ca35fa44538840728
spearstring <pchhkx@gmail.com>
2022-05-31 17:41:41 +0900

    220531 WPF 학습

---
+++ day02/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Media;
+
+namespace WpfBikeShop
+{
+    public class Car
+    {
+        public double Speed { get; set; }
+        public Color Color { get; set; }
+        public Human Driver { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        
+    }
+
+    public class Human
+    {
+        public string FirstName { get; set; }
+        public bool HasDrivingLicense { get; set; }
+    }
+}
