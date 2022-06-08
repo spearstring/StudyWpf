5ce51c1b3b2957d3505f043c4ce8b50b4764922b
spearstring <pchhkx@gmail.com>
2022-06-08 17:37:44 +0900

    220608 커밋

---
+++ day06/DummyDataTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyDataTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var respository = new SampleRepository();
+            var customers = respository.GetCustomers();
+
+            Console.WriteLine(JsonConvert.SerializeObject(customers, Formatting.Indented));
+            //Console.WriteLine(customers);
+        }
+    }
+}
---
+++ portfolio/WpfApp1/WpfPortfolio/DummyDataApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DummyDataApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var Rooms = new[] { "DINNING", "LIVING", "BATH", "BED" };  // 부엌, 거실, 욕실, 침실
+
+                var SeneorDummy = new Faker<SensorInfo>()
+                    .RuleFor(r => r.DevId, f => f.PickRandom(Rooms))
+                    .RuleFor(r => r.CurrTime, f => f.Date.Past(0))
+                    .RuleFor(r => r.Temp, f => f.Random.Float(19.0f, 30.9f))
+                    .RuleFor(r => r.Humid, f => f.Random.Float(40.0f, 63.9f));
+
+                var currValue = SeneorDummy.Generate();
+
+                Console.WriteLine(JsonConvert.SerializeObject(currValue, Formatting.Indented));
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
